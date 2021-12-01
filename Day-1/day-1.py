fadc40c624a24fd1ff71d32de8341657a35b5d9e
rufenmatt <mattia.ruefenacht@gmail.com>
2021-12-01 07:44:55 +0100

    first day

---
+++ Day-1/day-1.py
@@ -0,0 +1,27 @@
+import os
+
+os.getcwd()
+
+os.chdir(r'/Users/mattiarufenacht/Desktop/AOC2021/Day-1')
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    numbers = [int(line.strip()) for line in lines]
+
+def part1(numbers):
+    higher = 0
+    for i in range(len(numbers)):
+        if (numbers[i] > numbers[i-1]):
+            higher += 1
+    return higher
+
+def part2(numbers):
+    higher = 0
+    for i in range(len(numbers) - 2):
+        if ((numbers[i] + numbers[i+1] + numbers[i+2]) > (numbers[i-1] + numbers[i] + numbers[i+1])):
+            higher += 1
+    return higher
+
+if __name__ == "__main__":
+    print('Part 1 solution:', part1(numbers))
+    print('Part 2 solution:', part2(numbers))
